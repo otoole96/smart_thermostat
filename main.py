92ced95bb734fc90bf693e65b697dd11e735eab0
Tom <krenelka1151@gmail.com>
2019-04-07 13:59:31 -0400

    added header

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Main Function
+#-------------------------------------------------------------------------
+#Program Name: main.py
+#Author: Thomas Krenelka, Zach O'Toole
+#------------------------------------------------------------------------
+#Description:
+#   This the main routine that calls all other subroutines.
+#   Main entry point for the application.
+
+#-----------------------------------------------------------------------
