a5e7a43c08f7cccc47a956b38eea75028f7fde9d
TomVsCapstone <49373189+TomVsCapstone@users.noreply.github.com>
2019-04-07 19:36:01 -0400

    Fixed function arguments

--- bounds.py
+++ bounds.py
@@ -12,28 +12,29 @@
 #   occ_hist
 #   prob_present
 
-def init():
-    a = 0.5         #a + b must equal 1 
-    b = 0.5
-    occ_sum = 0
-    bound_shift = 0  #do not make this more than 5 degrees
 
-def outside_comp():
+a = 0.5         #a + b must equal 1
+b = 0.5
+occ_sum = 0
+bound_shift = 0  #do not make this more than 5 degrees ever
+
+def outside_comp(outside_t,inside_t):
     if outside_t > inside_t:
         case = 0 #hot
     else:
         case = 1 #cold
 
-def update_sum():
+def update_sum(occ):
     if occ == 1 and occ_sum < 8:
         occ_sum = occ_sum+1
     elif occ == 0 and occ_sum > 0:
         occ_sum = occ_sum - 1
 
-def set_new()
-    outside_comp()
-    update_sum()
+def set_new(outside_t,inside_t,occ)
+    outside_comp(outside_t,inside_t)
+    update_sum(occ)
     boundshift = ((4-0.5*occ_sum)*a)+((4 - prob_present)*b)
     setpoint = user_setpoint + boundshift*(-1)^case
+
 
 
