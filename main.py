64554953309ad34e84dcb28e332773458c17f461
Zachary O'Toole <toole96@gmail.com>
2019-04-07 15:00:30 -0400

    added some imports and main logic

--- main.py
+++ main.py
@@ -7,3 +7,27 @@
 #   Main entry point for the application.
 
 #-----------------------------------------------------------------------
+
+import pid_control.py as pid
+import bounds.py as bounds
+import thermostat_io.py as io
+import learning as smart
+import 
+
+# Main function of the project
+def main():
+    inside_t, humidity, user_setpoint, occ, outside_t = io.get_inputs()
+    occ_hist = smart.load_history()
+
+    prob_present = smart.probability_present(occ_hist)
+
+    setpoint = bounds.determine_setpoint(occ, prob_present, user_setpoint)
+
+    pid.set_relays(setpoint, inside_t)
+    
+
+    
+if __name__ == "__main__ ":
+    main()
+
+
