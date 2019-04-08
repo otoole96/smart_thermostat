0139ca48d9946b84a66e4b4d0c7cb7e170814c95
TomVsCapstone <49373189+TomVsCapstone@users.noreply.github.com>
2019-04-08 12:01:06 -0400

    added loop

--- main.py
+++ main.py
@@ -16,17 +16,14 @@ import learning as smart
 
 # Main function of the project
 def main():
-    inside_t, humidity, user_setpoint, occ, outside_t = io.get_inputs()
-
-    occ_hist = smart.load_history()
-
-    prob_present = smart.probability_present(occ_hist)
-
-    setpoint = bounds.determine_setpoint(occ, prob_present, user_setpoint)
-
-    pid.set_relays(setpoint, inside_t)
-    
+    heat = 0
+    ac = 0
+    fan = 0
     while True:
+        inside_t, humidity, user_setpoint, occ, outside_t = io.get_inputs()
+        occ_hist = smart.load_history()
+        prob_present = smart.probability_present(occ_hist)
+        setpoint = bounds.determine_setpoint(occ, prob_present, user_setpoint)
         heat, ac, fan = bang_bang.bang_bang(heat, ac, fan, setpoint,inside_t,outside_t)
 
     
