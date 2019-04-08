5f2c3c365910bc6f9acd58309d11241114cb7039
Zachary O'Toole <toole96@gmail.com>
2019-04-08 13:04:04 -0400

    new file for getting IO

---
+++ thermostat_inputs.py
@@ -0,0 +1,87 @@
+# Thermostat IO
+# Purpose: to get the inputs from the thermostat sensors and output stuff.
+#
+#
+#
+
+import os
+import glob
+import time
+import datetime
+
+from urllib.request import urlopen
+import json
+
+#---------------------------------------------------------------------------------------------------
+# Inside Temp.
+#---------------------------------------------------------------------------------------------------
+
+# Helper function for reading the temperature
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+# Reads the temp from the sensor
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0        
+        return  temp_c, temp_f
+
+# Main entry point for getting the inside temperature
+def get_inside_temp(inside_t):
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+
+    temp_c, temp_f = read_temp()
+    print(str(temp_c))	
+    print(str(temp_f))	
+    return temp_f
+
+#---------------------------------------------------------------------------------------------------
+# Outside Temp.
+#---------------------------------------------------------------------------------------------------
+
+# Queries the open weather api for the weather
+def query_outside_temp ():
+    apikey="ac918d16f3698465e610f6e08518cced"
+    url="http://api.openweathermap.org/data/2.5/weather?zip=43201,us&appid=ac918d16f3698465e610f6e08518cced"
+    meteo=urlopen(url).read()
+    meteo = meteo.decode('utf-8')
+    weather = json.loads(meteo)
+    outsidetemp = 0
+    for key, value in weather['main'].items():
+        if key == "temp":
+            outsidetemp = value        
+    print(outsidetemp)
+    return outsidetemp
+
+# Main entry point for getting the outside temp
+def get_outside_t (outside_t):
+    outside_t = query_outside_temp()
+    return outside_t
+
+
+#---------------------------------------------------------------------------------------------------
+# Occupancy
+#---------------------------------------------------------------------------------------------------
+
+# Helper function for the occupancy
+def ping_the_user():
+
+
+# Main entry point for obtaining the occupancy
+def get_occ(occ):
+    occ = ping_the_user()
+    return occ
