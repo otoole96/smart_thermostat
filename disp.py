168a4898cf916cfd8c41e292d1bba9732dd5a035
Drake Shafer <drake.shafer@gmail.com>
2019-04-17 20:01:39 -0400

    Actually fix resolution

--- disp.py
+++ disp.py
@@ -41,8 +41,8 @@ screen = pygame.display.set_mode((480,320))
 pygame.display.set_caption('Thermostat')
 screen.fill(BLACK)
 # Triangles
-up_arr = pygame.draw.polygon(screen, GREEN, ((600, 150), (700, 290), (500, 290)))
-down_arr = pygame.draw.polygon(screen, GREEN, ((600, 450), (700, 310), (500, 310)))
+up_arr = pygame.draw.polygon(screen, GREEN, ((360, 80), (420, 140), (300, 140)))
+down_arr = pygame.draw.polygon(screen, GREEN, ((360, 240), (420, 180), (300, 180)))
 
 # --------------------------------------------
 #   Text Display Function
@@ -61,33 +61,33 @@ def update_disp():
     # Clear screen
     screen.fill(BLACK)
     # Buttons
-    pygame.draw.polygon(screen, GREEN, ((600, 150), (700, 290), (500, 290)))
-    pygame.draw.polygon(screen, GREEN, ((600, 450), (700, 310), (500, 310)))
+    pygame.draw.polygon(screen, GREEN, ((360, 80), (420, 140), (300, 140)))
+    pygame.draw.polygon(screen, GREEN, ((360, 240), (420, 180), (300, 180)))
     # Outside Temp Label
-    disp_text(screen, small_font, 'Outside:', DARK_GRAY, (100, 525))
+    disp_text(screen, small_font, 'Outside:', DARK_GRAY, (60, 280))
     # Outside Temp
-    disp_text(screen, medium_font, str(main_globals.outside_t), DARK_GRAY, (300, 525))
+    disp_text(screen, medium_font, str(main_globals.outside_t), DARK_GRAY, (180, 280))
     # Inside Temp Label
-    disp_text(screen, small_font, 'Temperature:', DARK_GRAY, (100, 75))
+    disp_text(screen, small_font, 'Temperature:', DARK_GRAY, (60, 40))
     # Deg Symbol
-    disp_text(screen, small_font, 'o', WHITE, (365, 165))
+    disp_text(screen, small_font, 'o', WHITE, (210, 80))
     # Temp Unit
-    disp_text(screen, small_font, 'F', WHITE, (365, 375))
+    disp_text(screen, small_font, 'F', WHITE, (210, 240))
     # Inside Temp
-    disp_text(screen, large_font, str(main_globals.inside_t), WHITE, (200, 300))
+    disp_text(screen, large_font, str(main_globals.inside_t), WHITE, (120, 160))
     # Setpoint Label
-    disp_text(screen, small_font, 'Set:', DARK_GRAY, (500, 75))
+    disp_text(screen, small_font, 'Set:', DARK_GRAY, (300, 40))
     # Setpoint Display
-    disp_text(screen, medium_font, str(main_globals.user_setpoint), DARK_GRAY, (700, 75))
+    disp_text(screen, medium_font, str(main_globals.user_setpoint), DARK_GRAY, (420, 40)
     # AC label
-    disp_text(screen, small_font, 'AC:', DARK_GRAY, (450, 525))
+    disp_text(screen, small_font, 'AC:', DARK_GRAY, (270, 280))
     # Furnace label
-    disp_text(screen, small_font, 'Heat:', DARK_GRAY, (600, 525))
+    disp_text(screen, small_font, 'Heat:', DARK_GRAY, (390, 280))
     # AC and Furnace Indicators
     if main_globals.ac == 1 :
-        disp_text(screen, small_font, 'ON', DARK_GRAY, (525, 525))
+        disp_text(screen, small_font, 'ON', DARK_GRAY, (315, 280))
     if main_globals.heat == 1 :
-        disp_text(screen, small_font, 'ON', DARK_GRAY, (700, 525))
+        disp_text(screen, small_font, 'ON', DARK_GRAY, (435, 280))
 
 
 def ui_main():
