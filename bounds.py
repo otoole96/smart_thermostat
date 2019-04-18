2d1efd3c143c828e8bc9e9b810de34abd07a3ab7
TomVsCapstone <49373189+TomVsCapstone@users.noreply.github.com>
2019-04-18 19:27:05 -0400

    Update bounds.py

--- bounds.py
+++ bounds.py
@@ -14,12 +14,14 @@
 
 import main_globals
 
+#determine the mode of the thermostat. 
 def outside_comp():
     if main_globals.outside_t > main_globals.setpoint:
         main_globals.mode = 0 #hot outside/AC in use
     else:
         main_globals.mode = 1 #cold outside/Heat in use
-
+        
+#Increment or decrement the count of occupancy samples within the last two hours that user was home
 def update_sum():
     if main_globals.occ == 1 and main_globals.occ_sum < 8:
         main_globals.occ_sum = main_globals.occ_sum + 1
@@ -29,11 +31,12 @@ def update_sum():
     
     print(main_globals.occ_sum)
 
+#Calculate the bound shift and apply it to the back-end setpoint    
 def set_new():
-    a = 0.5
-    b = 0.5
+    a = 0.5         #Recent occupancy weight
+    b = 0.5         #Machine learning weight 
     outside_comp()
     update_sum()
     boundshift = ((4-0.5*main_globals.occ_sum)*a)+((4 - 4*main_globals.prob_present)*b) #cant go above 5
-    main_globals.setpoint = main_globals.user_setpoint + boundshift*(-1)**main_globals.mode
+    main_globals.setpoint = main_globals.user_setpoint + boundshift*(-1)**main_globals.mode #-1^mode determines which direction the bound shift moves
     print("Setpoint = " + str(main_globals.setpoint))
